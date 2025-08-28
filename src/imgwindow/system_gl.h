b49f92bcf59445058f17752ce156de59cb1c02f8
Justin <6639835@gmail.com>
2025-08-28 10:26:58 +0800

    Add additional warning suppressions in CMakeLists.txt for MSVC builds and update OpenGL header inclusion in system_gl.h for Windows compatibility. This improves build clarity and ensures proper OpenGL functionality across platforms.

--- src/imgwindow/system_gl.h
+++ src/imgwindow/system_gl.h
@@ -5,6 +5,10 @@
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION
 #include <OpenGL/gl.h>
+#elif defined(_WIN32)
+#include <GL/gl.h>
+// Note: GL/glext.h is not commonly available on Windows
+// X-Plane SDK provides necessary OpenGL functionality
 #else
 #include <GL/gl.h>
 #include <GL/glext.h>
